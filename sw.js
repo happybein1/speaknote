f8f39fae183b3ac0fb200ca418287858e857d2ba
happybein1 <happybein1@gmail.com>
2026-05-13 08:18:39 +0200

    first commit

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'speaknote-v1-0-20260512-1';
+const STATIC_ASSETS = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(STATIC_ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(names => Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then(c => c || fetch(e.request).catch(() => caches.match('/index.html'))));
+});
